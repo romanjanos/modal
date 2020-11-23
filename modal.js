43e5b6256022f8f4b997591c86e18cb90e012648
Janos <romanjanos0504@gmail.com>
2020-11-23 08:15:25 +0100

    changed 1

--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
 function addClassToBody(class_name) {
     let element = document.querySelector('body');
-    let classNames = element.className.split(" ");
+    let classNames = element.className.split('');
     if (classNames.indexOf(class_name) == -1) {
         element.className += " " + class_name;
     }
@@ -12,13 +12,14 @@ function removeClassFromBody(class_name) {
 }
 
 let button = document.querySelector('.btn');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     addClassToBody('modal__open');
 });
 
 
-let closeButton = document.querySelector('.modal__button__header');
-closeButton.addEventListener('click', function(){
-    removeClassFromBody('modal__open');
-});
-
+let closeButton = document.querySelectorAll('.modal__button__header, .footer__button');
+for (let i = 0; i < closeButton.length; i += 1) {
+    closeButton[i].addEventListener('click', function () {
+        removeClassFromBody('modal__open');
+    });
+}
